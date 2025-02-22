28486f592f08ae9ef5e4b307b9a6b661ab4db29f
BraydenUppal <buppal@scu.edu>
2025-02-21 19:13:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const quizData = [
     {
-      question: 'What is the capital of France?',
-      options: ['Paris', 'London', 'Berlin', 'Madrid'],
-      answer: 'Paris',
+      question: 'Adrian Garica',
+      options: ['MECH', 'ECEN', 'CSEN', 'CENG'],
+      answer: 'MECH',
     },
     {
       question: 'What is the largest planet in our solar system?',
